2569460ec30112d242ea58abe3238bf7daa1146b
Camilo Espinosa <espinosacamilop1@gmail.com>
2021-07-29 22:16:36 -0400

    test

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
 
-
-window.onload() = () =>{
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d')
 
 const game = new Game(canvas, context);
 
 game.start();
-}
